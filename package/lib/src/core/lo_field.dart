51b68193ee0c68d1cceda0e3e7ce3a2f2334ce5d
Leviatanus <79842334+Leviatanus@users.noreply.github.com>
2022-09-01 01:34:20 +0200

    fix: correct fields tab traverse (#11)

--- package/lib/src/core/lo_field.dart
+++ package/lib/src/core/lo_field.dart
@@ -51,16 +51,16 @@ class _LoFieldState<TKey, TValue> extends State<LoField<TKey, TValue>> {
   @override
   Widget build(BuildContext context) {
     final state = LoScope.of<TKey>(context);
-
-    return FocusScope(
-      child: Focus(
-        onFocusChange: (focus) => state.onFieldFocusChanged<TValue>(
-          widget.loKey,
-          focus,
-        ),
-        child: widget.builder(
-          state.fields.get<TValue>(widget.loKey),
-        ),
+    // Focus is needed to use onFocusChanged to mark fields as touched
+    return Focus(
+      // Ignore this Focus because LoFields already have their own
+      skipTraversal: true,
+      onFocusChange: (focus) => state.onFieldFocusChanged<TValue>(
+        widget.loKey,
+        focus,
+      ),
+      child: widget.builder(
+        state.fields.get<TValue>(widget.loKey),
       ),
     );
   }
